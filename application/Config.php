0590ce08365653f032b8b4341d2b6f697f18e23d
estebananca <esteban.ancamil100@gmail.com>
2025-01-06 00:56:23 -0300

    Add files via upload

---
+++ application/Config.php
@@ -0,0 +1,19 @@
+<?php
+
+define('BASE_URL', 'http://localhost/noticias_OK/');
+define('DEFAULT_CONTROLLER', 'index');
+define('DEFAULT_LAYOUT', 'default');
+define('APP_NAME', 'NOTICIAS_APP');
+
+define('SESSION_TIME', 10);
+define('HASH_KEY', 'ec13oGBHFWD3twF8wT4ifaVTxeEdLbv8CVOjGYL98Mo=');
+define('CTRL', 'cQEqm3uDBUxFH+4VI8y6jqSjP/51tuyvNvNSbHpcqgY=');
+
+define('DB_DRIVER', 'mysql');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'noticias');
+define('DB_CHAR', 'utf8');
+define('METHODENCRIPT','AES-256-ECB');
+define('KEY','APP_NOTICIAS');
